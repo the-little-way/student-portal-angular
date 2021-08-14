67137e271a3ca1a39bfcb32c32a02f6e12fbc558
William <80454082+the-little-way@users.noreply.github.com>
2021-08-14 09:36:27 +0000

    Vroom!!! Away we go...

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
